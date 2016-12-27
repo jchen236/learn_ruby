ea0a51e519797c09b4d38bacda005e3d1336b3ae
Jimmy Chen <jzc8@cornell.edu>
2016-12-27 00:28:14 -0500

    Finish simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
 #write your code here
+def echo str
+  return str
+end
+
+def shout str
+  return str.upcase
+end
+
+def repeat (str, num = 2)
+  return (str + " ") * (num-1) + str
+end
+
+def start_of_word (str, n)
+  return str[0..n-1]
+end
+
+def first_word str
+  str_arr = str.split
+    return str_arr[0]
+  end
+
+def titleize str
+  str_arr = str.split
+  str_arr_cap = str_arr.map { |x| x.capitalize}
+  return str_arr_cap.join(' ')
+
+end
