33383952fd2c14517c7395a7e551530e912ac53e
Jimmy Chen <jzc8@cornell.edu>
2016-12-27 21:32:01 -0500

    Finish timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,34 @@
 class Timer
   #write your code here
+
+  def initialize
+    @seconds = 0
+  end
+
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(value)
+    @seconds = value
+  end
+
+  def pad_time time
+    if time < 10
+      return '0' + time.to_s
+    else
+      return time.to_s
+    end
+  end
+
+  def time_string
+    a = @seconds
+    hours = a/3600
+    a = a % 3600
+    minutes = a / 60
+    a = a % 60
+    seconds = a
+    return pad_time(hours) + ":" + pad_time(minutes) + ":" + pad_time(seconds)
+  end
 end
