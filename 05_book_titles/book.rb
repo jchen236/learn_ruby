4d8ae6dd8fb723896a09aefe965eda4009821baf
Jimmy Chen <jzc8@cornell.edu>
2016-12-27 21:12:25 -0500

    Finish assignment 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,33 @@
 class Book
 # write your code here
+def title=(value)
+  @title = value
+end
+
+
+def self.title
+  @title
+end
+
+
+def capitalize_word word
+  minor_words = ['the', 'a', 'and', 'in', 'of', 'an']
+  if minor_words.include? word
+    return word
+  else
+    return word.capitalize
+  end
+end
+
+def title
+  a = @title
+
+  word_array = a.split
+  new_word_array = word_array.map { |x| capitalize_word x}
+  new_word_array[0] = new_word_array[0].capitalize
+  return new_word_array.join(' ')
+end
+
+
+
 end
